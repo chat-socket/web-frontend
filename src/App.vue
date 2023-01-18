ab7fc329f23aa926b2d3144463fe29fa4b1c99d2
VU Manh Tu <glmanhtu@gmail.com>
2023-01-18 14:19:01 +0700

    make app height resizes when the window resizes
    From demon-bixia/Avian-Template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import { onMounted, onUnmounted, ref } from 'vue';
 import FadeTransition from './components/reusables/transitions/FadeTransition.vue';
 import {useSettingsStore} from "./stores/settings";
 
@@ -10,14 +11,26 @@ const settings = useSettingsStore();
 // todo refactor tailwind classes
 
 
-const vh = () => {
-    return `${window.innerHeight}px`;
+// the app height
+const height = ref(`${window.innerHeight}px`);
+
+// change the app height whenever the window resizes
+const resizeWindow = () => {
+    height.value = `${window.innerHeight}px`;
 };
+// add the resize event when the component mounts
+onMounted(() => {
+    window.addEventListener('resize', resizeWindow);
+});
+// remove the event when un-mounting the component
+onUnmounted(() => {
+    window.removeEventListener('resize', resizeWindow);
+});
 </script>
 
 <template>
     <div :class="{ 'dark': settings.isDarkMode }">
-        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{ height: vh() }">
+        <div class="bg-white dark:bg-gray-800 transition-all duration-500" :style="{ height: height }">
             <router-view v-slot="{ Component }">
                 <FadeTransition>
                     <component :is="Component" />
