4ee5200b6526fb6b8d22647af958d9dd7b7ed01c
VU Manh Tu <glmanhtu@gmail.com>
2023-01-23 15:08:13 +0700

    Adding docker file for Tilt and configure vite accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  logLevel: 'warn',
   plugins: [vue()],
   server: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 4200
   }
 })
